e02233fb2921712249deac1176aa58d57054c027
Luana Schorr <luanaschorr02@gmail.com>
2024-11-06 20:36:15 -0300

    arquivos atualizados

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package trabalhoBiblioteca;
+
+/**
+ *
+ * @author luana
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Autores autor = new Autores();
+        autor.cadastrarAutores();    
+    }
+}
